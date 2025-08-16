0e527c3198c4e02ef69b917607086a95e6b1d4d3
Mario Alvarado <m_alvaradox@hotmail.com>
2025-08-16 13:48:35 -0500

    frontend - init

---
+++ frontend/lib/api.dart
@@ -0,0 +1,56 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class EcoClickAPI {
+  // Si en Windows te falla "localhost", usa 127.0.0.1
+  static const String baseUrl = 'http://127.0.0.1:4000';
+
+  static Future<List<dynamic>> getQuizzes() async {
+    final res = await http.get(Uri.parse('$baseUrl/quizzes'));
+    if (res.statusCode >= 400)
+      throw Exception('Error ${res.statusCode}: ${res.body}');
+    final data = jsonDecode(res.body) as Map<String, dynamic>;
+    return (data['items'] as List<dynamic>);
+  }
+
+  static Future<Map<String, dynamic>> getQuiz(String id) async {
+    final res = await http.get(Uri.parse('$baseUrl/quizzes/$id'));
+    if (res.statusCode >= 400)
+      throw Exception('Error ${res.statusCode}: ${res.body}');
+    final data = jsonDecode(res.body) as Map<String, dynamic>;
+    return (data['item'] as Map<String, dynamic>);
+  }
+
+  static Future<Map<String, dynamic>> postAnswers({
+    required String quizId,
+    required int userId,
+    required List<Map<String, dynamic>> answers,
+    required int score,
+    required int timeSec,
+  }) async {
+    final res = await http.post(
+      Uri.parse('$baseUrl/games/$quizId/answers'),
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({
+        'userId': userId,
+        'answers': answers,
+        'score': score,
+        'timeSec': timeSec,
+      }),
+    );
+    if (res.statusCode >= 400)
+      throw Exception('Error ${res.statusCode}: ${res.body}');
+    return jsonDecode(res.body) as Map<String, dynamic>;
+  }
+
+  static Future<List<dynamic>> getFeedback({String? topic}) async {
+    final url = topic == null || topic.isEmpty
+        ? '$baseUrl/feedback'
+        : '$baseUrl/feedback?topic=$topic';
+    final res = await http.get(Uri.parse(url));
+    if (res.statusCode >= 400)
+      throw Exception('Error ${res.statusCode}: ${res.body}');
+    final data = jsonDecode(res.body) as Map<String, dynamic>;
+    return (data['items'] as List<dynamic>);
+  }
+}
