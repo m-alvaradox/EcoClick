9b51421779ed58fef2657dc9f50534a19939672b
Mario Alvarado <m_alvaradox@hotmail.com>
2025-08-23 21:07:50 -0500

    Update prefers.dart

--- frontend/lib/utils/prefers.dart
+++ frontend/lib/utils/prefers.dart
@@ -1,6 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:frontend/screens/login_page.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 Future<String> getUsername() async {
   final prefs = await SharedPreferences.getInstance();
   return prefs.getString('username') ?? 'Eco-Héroe';
+}
+
+Future<void> logoutAndGoToLogin(BuildContext context) async {
+  final prefs = await SharedPreferences.getInstance();
+  await prefs.remove('username');
+
+  if (context.mounted) {
+    Navigator.pushAndRemoveUntil(
+      context,
+      MaterialPageRoute(builder: (_) => const LoginPage()),
+      (route) => false,
+    );
+  }
 }
